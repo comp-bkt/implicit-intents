63cc0b2c4ebcccd6a8deddbb23e935496cdf37d8
pat <p@p>
2023-01-11 10:34:54 +0000

    xml view added, onClick defined as xml attributes, locat message outputted

--- app/src/main/java/com/example/implicitintents/MainActivity.kt
+++ app/src/main/java/com/example/implicitintents/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.implicitintents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun openWebsite(view: View) {
+        Log.v(TAG, "open website")
+    }
+
+    fun openLocation(view: View) {
+        Log.v(TAG, "open location")
+    }
+
+    fun shareText(view: View) {
+        Log.v(TAG, "share text")
+    }
+
+    companion object {
+        private const val TAG:String = "Implicit_Intents"
+    }
+
 }
