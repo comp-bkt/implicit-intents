ddccd6fd18dc80c3c57759334d8018564f490991
pat <p@p>
2023-01-12 20:22:44 +0000

    added code for open webpage

--- app/src/main/java/com/example/implicitintents/MainActivity.kt
+++ app/src/main/java/com/example/implicitintents/MainActivity.kt
@@ -1,18 +1,36 @@
 package com.example.implicitintents
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mWebsiteEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mWebsiteEditText = findViewById(R.id.website_edittext)
     }
 
     fun openWebsite(view: View) {
         Log.v(TAG, "open website")
+
+        val url = mWebsiteEditText.text.toString()
+
+        val webpage = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Log.d(TAG, "Can't handle this!")
+        }
     }
 
     fun openLocation(view: View) {
